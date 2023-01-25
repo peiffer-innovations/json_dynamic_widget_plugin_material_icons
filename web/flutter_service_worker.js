d2810547b071fa4dfdabad855aa1051dce8f1b32
action <noop@github.com>
2023-01-25 03:36:56 +0000

    Pages 56

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,24 +3,23 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
-"index.html": "c95a0235ca482b95f092cf1b092745f4",
-"/": "c95a0235ca482b95f092cf1b092745f4",
-"main.dart.js": "449044d38fe631b5a6c121879137a6be",
+  "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
+"index.html": "6d3600a3af6f9fa56e71db81b25b8fa1",
+"/": "6d3600a3af6f9fa56e71db81b25b8fa1",
+"main.dart.js": "0e17aa986feafc11189e6aa780c53b25",
 "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
-"assets/NOTICES": "cd8c09e1508d8925d7e1388886727f26",
-"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/shaders/ink_sparkle.frag": "59f1e52ba59fd6c96cc822ff3e5a3de9",
+"assets/NOTICES": "5b51a59b3e65fd0e72b241bee6614944",
+"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/assets/pages/material_icons.json": "fa12eb0cf99b012476166ceaa9100695",
 "assets/AssetManifest.json": "73a6ffc8c7c7bc0b677253bfcf932916",
-"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
-"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
-"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
-"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "version.json": "ff966ab969ba381b900e61629bfb9789"
 };
 
